fdd6c599cbc7317fe9d8c67c2685f098f859a625
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-13 00:40:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
-import pandas as pd
+from scipy.stats import t
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 356550601 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(X) -> bool:
+    n = len(X)
+    mean = np.mean(X)
+    std = np.std(X, ddof=1)
+    threshold = 500
+    alpha = 0.1
+    t_stat = (mean - threshold) / (std / np.sqrt(n))
+    t_crit = t.ppf(1 - alpha, n - 1)
+    return t_stat > t_crit
